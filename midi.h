4144fa42fcc80b25fc2f75b7159333cf75292e9d
LudvigStjernman <118138748+LudvigStjernman@users.noreply.github.com>
2022-11-17 04:26:41 +0100

    Update midi.h

--- midi.h
+++ midi.h
@@ -1,11 +1,11 @@
-#include "RtMidi.h"
-#include <thread>
+#include "RtMidi.h" //midi-library
+#include <thread> //library för att skapa trådar
 struct midi
 {
-    RtMidiIn *midin;
-    std::string portnamn = "";
-    unsigned int portnr = 0;
-    void midicallback(double, std::vector<unsigned char>*, void*);
-    void cleanup();
-    midi();
+    RtMidiIn *midin; //midi-mottagar-objekt
+    std::string portnamn = ""; //namn för midi-port
+    unsigned int portnr = 0; //nummer för port
+    void midicallback(double, std::vector<unsigned char>*, void*); //funktion som tillkallas för varje not
+    void cleanup(); //friar allt minne
+    midi(); //kontruktör
 };
